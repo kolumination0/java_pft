a8f6fdaa7002cb227aa64927764d9224aa9a28ab
Oleg Platonov <77516081+kolumination0@users.noreply.github.com>
2021-08-22 19:41:08 +0300

    Создан первый автотест

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -6,24 +6,16 @@ public class MyFirstProgram {
 
 		hello("Oleg");
 
-		double s = 8;
-		System.out.println("Площадь квадрата со сторонами " + s + " = " + Square(s));
+		Square s = new Square(9);
+		System.out.println("Площадь квадрата со сторонами " + s.l + " = " + s.area());
 
-		double a = 8;
-		double b = 6;
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + Rectangle(a, b));
+		Rectangle r = new Rectangle(8, 6);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 
 	public static void hello(String s) {
 		System.out.println("Hello, " + s + "!");
 	}
 
-	public static double Square(double a) {
-		return a * 4;
-	}
-
-	public static double Rectangle(double a, double b) {
-		return a * b;
-	}
 }
 
